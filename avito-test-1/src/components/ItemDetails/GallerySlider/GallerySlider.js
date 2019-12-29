6d52b4eee46bc983e6977269ec3f09a91c94309c
drmrhdt <drmrhdt@gmail.com>
2019-12-29 17:38:08 +0300

    Add style to ItemDetails, GallerySlider, add working prev/next buttons in slider, hover gallery item

--- avito-test-1/src/components/ItemDetails/GallerySlider/GallerySlider.js
+++ avito-test-1/src/components/ItemDetails/GallerySlider/GallerySlider.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
+import classNames from "classnames";
 import styles from "./GallerySlider.module.scss";
 
 export default class GallerySlider extends Component {
@@ -15,26 +16,66 @@ export default class GallerySlider extends Component {
     selected: 0
   };
 
+  next = () => {
+    const { images } = this.props;
+    this.setState(prevState => ({
+      selected: (prevState.selected + 1) % images.length
+    }));
+  };
+
+  prev = () => {
+    const { images } = this.props;
+    this.state.selected === 0
+      ? this.setState({ selected: images.length - 1 })
+      : this.setState(prevState => ({
+          selected: (prevState.selected - 1) % images.length
+        }));
+  };
+
+  setImageFromGalleryToSlider = e => {
+    this.setState({ selected: e.currentTarget.dataset.index });
+  };
+
   render() {
     const { images, title } = this.props;
     const { selected } = this.state;
     return (
       <div>
         <div className={styles.slider}>
-          <button className={styles.slider__btn}>{"<"}</button>
+          <button
+            className={classNames(styles.slider__btn, styles.slider__btn_prev)}
+            onClick={this.prev}
+          ></button>
           <img
             className={styles.slider__image}
             src={images[selected]}
             alt={title}
           />
-          <button className={styles.slider__btn}>{">"}</button>
+          <button
+            className={classNames(styles.slider__btn, styles.slider__btn_next)}
+            onClick={this.next}
+          ></button>
         </div>
         <ul className={styles.gallery}>
-          {images.map(image => (
-            <li className={styles.gallery__item} key={image} title={title}>
-              <img className={styles.gallery__image} src={image} alt={title} />
-            </li>
-          ))}
+          {images &&
+            images.map((image, index) => (
+              <li
+                className={classNames(
+                  styles.gallery__item,
+                  index === +selected ? styles.selected : styles.not_selected
+                )}
+                key={image}
+                title={title}
+              >
+                <img
+                  className={styles.gallery__image}
+                  src={image}
+                  alt={title}
+                  data-index={index}
+                  onMouseOver={this.setImageFromGalleryToSlider}
+                />
+              </li>
+            ))}
         </ul>
       </div>
     );
--- avito-test-1/src/components/ItemDetails/ItemDetails.js
+++ avito-test-1/src/components/ItemDetails/ItemDetails.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
+import { NavLink } from "react-router-dom";
 import GallerySlider from "./GallerySlider";
 import { getItem } from "../../utilities/fetch";
 import styles from "./ItemDetails.module.scss";
@@ -42,17 +43,23 @@ export default class ItemDetails extends Component {
 
     return (
       <div className={styles.details}>
-        <GallerySlider
-          className={styles.details__slider}
-          images={images}
-          title={title}
-        />
-        <div className={styles.details__text}>
-          <p>{title}</p>
-          <p>{price}</p>
-          <p>{description}</p>
-          <p>{address}</p>
-          <p>{sellerName}</p>
+        <NavLink className={styles.details_btn_back} to="/">
+          Назад
+        </NavLink>
+        <div className={styles.details__content}>
+          <GallerySlider
+            className={styles.details__slider}
+            images={images}
+            title={title}
+          />
+          <div className={styles.details__text}>
+            <h1 className={styles.details__title}>{title}</h1>
+            <h2 className={styles.details__price}>{price}</h2>
+            <h3>Подробное описание:</h3>
+            <p>{description}</p>
+            <p>{address}</p>
+            <p className={styles.details__seller_name}>{sellerName}</p>
+          </div>
         </div>
       </div>
     );
